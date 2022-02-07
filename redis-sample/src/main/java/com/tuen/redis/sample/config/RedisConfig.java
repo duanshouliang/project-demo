ed94d9797100abf1ec136333da2cae67505acd36
duanshuliang <shuliang.duan@ecarx.com.cn>
2022-02-07 23:37:52 +0800

    2022-02-08

--- redis-sample/src/main/java/com/tuen/redis/sample/config/RedisConfig.java
+++ redis-sample/src/main/java/com/tuen/redis/sample/config/RedisConfig.java
@@ -1,26 +1,50 @@
 package com.tuen.redis.sample.config;
 
 import com.alibaba.fastjson.support.spring.GenericFastJsonRedisSerializer;
-import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.boot.autoconfigure.AutoConfigureAfter;
 import org.springframework.boot.autoconfigure.data.redis.RedisAutoConfiguration;
+import org.springframework.cache.annotation.CachingConfigurerSupport;
+import org.springframework.cache.interceptor.KeyGenerator;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.cache.RedisCacheConfiguration;
+import org.springframework.data.redis.cache.RedisCacheManager;
+import org.springframework.data.redis.cache.RedisCacheWriter;
 import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
 import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.serializer.RedisSerializationContext;
 import org.springframework.data.redis.serializer.StringRedisSerializer;
 
 import java.io.Serializable;
+import java.time.Duration;
 
 @Configuration
 @AutoConfigureAfter(RedisAutoConfiguration.class)
-public class RedisConfig {
+public class RedisConfig extends CachingConfigurerSupport {
     @Bean
     public RedisTemplate<String, Serializable> redisTemplate( LettuceConnectionFactory lettuceConnectionFactory){
         RedisTemplate<String, Serializable> redisTemplate = new RedisTemplate<>();
         redisTemplate.setKeySerializer(new StringRedisSerializer());
         redisTemplate.setValueSerializer(new GenericFastJsonRedisSerializer());
+        redisTemplate.setHashKeySerializer(new StringRedisSerializer());
+        redisTemplate.setHashValueSerializer(new GenericFastJsonRedisSerializer());
         redisTemplate.setConnectionFactory(lettuceConnectionFactory);
         return redisTemplate;
+    }
+
+    @Override
+    public KeyGenerator keyGenerator() {
+        return super.keyGenerator();
+    }
+
+    @Bean
+    public RedisCacheManager redisCacheManager(RedisTemplate redisTemplate){
+        RedisCacheWriter redisCacheWriter = RedisCacheWriter.nonLockingRedisCacheWriter(redisTemplate.getConnectionFactory());
+        RedisCacheConfiguration redisCacheConfiguration = RedisCacheConfiguration.defaultCacheConfig()
+                //设置默认缓存到期时间为一个小时
+                .entryTtl(Duration.ofHours(1))
+                //自定义缓存存储编码格式，默认为二进制格式
+                .serializeValuesWith(RedisSerializationContext.SerializationPair.fromSerializer(redisTemplate.getValueSerializer()));
+        return new RedisCacheManager(redisCacheWriter, redisCacheConfiguration);
     }
 }
